ff45b95553c6ff7a87d3ba80d4cd8545c1320277
anumshaikh <anum.mujeebullah@gmail.com>
2020-10-26 23:05:12 +0530

    first commit

---
+++ src/main/java/com/anums/blogApp/DiscussPoint/DiscussPointApplication.java
@@ -0,0 +1,13 @@
+package com.anums.blogApp.DiscussPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscussPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscussPointApplication.class, args);
+	}
+
+}
